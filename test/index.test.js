6acabe3a7e25246f9fca0101e16ffce93f32e6bf
neverland <chenjiahan.jait@bytedance.com>
2025-07-08 12:54:18 +0800

    feat: add support for es2021 (#19)

--- test/index.test.js
+++ test/index.test.js
@@ -8,12 +8,15 @@ test('should get ECMA version correctly', () => {
 	// Edge
 	assert.strictEqual(browserslistToESVersion(['Edge >= 12']), 5);
 	assert.strictEqual(browserslistToESVersion(['Edge >= 15']), 2017);
+	assert.strictEqual(browserslistToESVersion(['Edge >= 79']), 2019);
+	assert.strictEqual(browserslistToESVersion(['Edge >= 80']), 2020);
+	assert.strictEqual(browserslistToESVersion(['Edge >= 85']), 2021);
 
 	// Firefox
 	assert.strictEqual(browserslistToESVersion(['firefox >= 50']), 5);
 	assert.strictEqual(browserslistToESVersion(['firefox >= 54']), 2017);
 	assert.strictEqual(browserslistToESVersion(['firefox >= 78']), 2019);
-	assert.strictEqual(browserslistToESVersion(['firefox >= 80']), 2020);
+	assert.strictEqual(browserslistToESVersion(['firefox >= 85']), 2021);
 
 	// Chrome
 	assert.strictEqual(browserslistToESVersion(['Chrome >= 33']), 5);
@@ -22,6 +25,7 @@ test('should get ECMA version correctly', () => {
 	assert.strictEqual(browserslistToESVersion(['Chrome >= 67']), 2018);
 	assert.strictEqual(browserslistToESVersion(['Chrome >= 73']), 2019);
 	assert.strictEqual(browserslistToESVersion(['Chrome >= 80']), 2020);
+	assert.strictEqual(browserslistToESVersion(['Chrome >= 85']), 2021);
 
 	// Opera
 	assert.strictEqual(browserslistToESVersion(['opera >= 30']), 5);
@@ -31,6 +35,7 @@ test('should get ECMA version correctly', () => {
 	assert.strictEqual(browserslistToESVersion(['opera >= 51']), 2018);
 	assert.strictEqual(browserslistToESVersion(['opera >= 60']), 2019);
 	assert.strictEqual(browserslistToESVersion(['opera >= 67']), 2020);
+	assert.strictEqual(browserslistToESVersion(['opera >= 71']), 2021);
 
 	// Samsung
 	assert.strictEqual(browserslistToESVersion(['samsung >= 4']), 5);
@@ -39,6 +44,7 @@ test('should get ECMA version correctly', () => {
 	assert.strictEqual(browserslistToESVersion(['samsung >= 8.2']), 2018);
 	assert.strictEqual(browserslistToESVersion(['samsung >= 11.1']), 2019);
 	assert.strictEqual(browserslistToESVersion(['samsung >= 13']), 2020);
+	assert.strictEqual(browserslistToESVersion(['samsung >= 14']), 2021);
 
 	// Safari
 	assert.strictEqual(browserslistToESVersion(['safari >= 10']), 2015);
@@ -82,7 +88,7 @@ test('should get ECMA version correctly', () => {
 			'last 1 firefox version',
 			'last 1 safari version',
 		]),
-		2020,
+		2021,
 	);
 	assert.strictEqual(
 		browserslistToESVersion([
@@ -105,7 +111,7 @@ test('should get ECMA version correctly', () => {
 	assert.strictEqual(browserslistToESVersion(['ios_saf 11']), 2017);
 
 	// https://github.com/browserslist/browserslist/issues/682
-	assert.strictEqual(browserslistToESVersion(['and_ff >= 78']), 2020);
-	assert.strictEqual(browserslistToESVersion(['and_chr >= 53']), 2020);
-	assert.strictEqual(browserslistToESVersion(['ChromeAndroid >= 53']), 2020);
+	assert.strictEqual(browserslistToESVersion(['and_ff >= 78']), 2021);
+	assert.strictEqual(browserslistToESVersion(['and_chr >= 53']), 2021);
+	assert.strictEqual(browserslistToESVersion(['ChromeAndroid >= 53']), 2021);
 });
