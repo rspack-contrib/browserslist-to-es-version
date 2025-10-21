3e9264084bb7ff4f6a0f37a2b1529f966a3efda4
Wei <fi3ework@gmail.com>
2025-10-21 15:27:10 +0800

    feat: add support for es2023 (#28)

--- src/index.ts
+++ src/index.ts
@@ -9,16 +9,17 @@ export type ESVersion =
   | 2019
   | 2020
   | 2021
-  | 2022;
+  | 2022
+  | 2023;
 
-// The minimal version for [es2015, es2016, es2017, es2018, es2019, es2020, es2021, es2022]
+// The minimal version for [es2015, es2016, es2017, es2018, es2019, es2020, es2021, es2022, es2023]
 const ES_VERSIONS_MAP: Record<string, number[]> = {
-  chrome: [51, 52, 57, 64, 73, 80, 85, 94],
-  edge: [15, 15, 15, 79, 79, 80, 85, 94],
-  safari: [10, 10.3, 11, 16.4, 17, 17, 17, 17],
-  firefox: [54, 54, 54, 78, 78, 80, 80, 93],
-  opera: [38, 39, 44, 51, 60, 67, 71, 80],
-  samsung: [5, 6.2, 6.2, 8.2, 11.1, 13, 14, 17],
+  chrome: [51, 52, 57, 64, 73, 80, 85, 94, 110],
+  edge: [15, 15, 15, 79, 79, 80, 85, 94, 110],
+  safari: [10, 10.3, 11, 16.4, 17, 17, 17, 17, 17],
+  firefox: [54, 54, 54, 78, 78, 80, 80, 93, 115],
+  opera: [38, 39, 44, 51, 60, 67, 71, 80, 96],
+  samsung: [5, 6.2, 6.2, 8.2, 11.1, 13, 14, 17, 21],
 };
 
 const aliases: Record<string, string> = {
@@ -36,7 +37,7 @@ export function browserslistToESVersion(browsers: string[]): ESVersion {
     ignoreUnknownVersions: true,
   });
 
-  let esVersion: ESVersion = 2022;
+  let esVersion: ESVersion = 2023;
 
   for (const item of projectBrowsers) {
     const pairs = item.split(' ');
@@ -82,6 +83,8 @@ export function browserslistToESVersion(browsers: string[]): ESVersion {
       esVersion = Math.min(2020, esVersion) as ESVersion;
     } else if (version < versions[7]) {
       esVersion = Math.min(2021, esVersion) as ESVersion;
+    } else if (version < versions[8]) {
+      esVersion = Math.min(2022, esVersion) as ESVersion;
     }
   }
 
